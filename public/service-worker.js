b1005c3ab626a292d0eb3a2f31cfb101294b852f
Julien Le Mee <julemee@gmail.com>
2024-09-14 17:45:00 -0400

    essai PWA

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/manifest.json',
+          '/stylesheets/application.css',
+          '/javascripts/application.js'
+        ]);
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });

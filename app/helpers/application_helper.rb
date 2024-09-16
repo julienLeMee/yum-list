ac3cacde1572a9119eda75bf6e1f6b386c41614b
Julien Le Mee <julemee@gmail.com>
2024-09-15 21:42:39 -0400

    new design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,9 @@ module ApplicationHelper
   def theme_color_for_page
     case [controller_name, action_name]
     when ['restaurants', 'new'],
+        ['restaurants', 'index'],
         ['restaurants', 'tested_restaurants'],
-        ['restaurants', 'untested-restaurants'],
+        ['restaurants', 'untested_restaurants'],
         ['users', 'show']
       '#F5F5F5' # Couleur spécifique pour ces contrôleurs et actions
     else

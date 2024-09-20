fe6b91eaf8f7cc12a1ed598f466b87f3098160eb
Julien Le Mee <julemee@gmail.com>
2024-09-19 21:48:42 -0400

    sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  # Configurer le serveur SMTP
+  # Configurer le serveur SMTP avec Sendgrid
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: 'apikey',

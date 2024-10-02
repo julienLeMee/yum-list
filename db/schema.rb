4f5b8886f72a4bd256b5de3118fcd15ed67a1a8b
Julien Le Mee <julemee@gmail.com>
2024-10-01 23:56:50 -0400

    horaires d'ouverture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_01_012833) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_01_223032) do
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "friend_id", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_01_012833) do
     t.integer "rating"
     t.text "opening_hours"
     t.string "google_place_id"
+    t.string "place_id"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 

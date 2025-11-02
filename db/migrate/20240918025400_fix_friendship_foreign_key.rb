6969a4da2f2c912841ef083037805edf3f4ddd3d
Julien Le Mée <julienlemee@macbookair.home>
2025-11-02 03:21:40 -0500

    Remove outdated migration for fixing friendship foreign key, as the foreign key reference has been updated in a previous commit.

--- db/migrate/20240918025400_fix_friendship_foreign_key.rb
+++
@@ -1,9 +0,0 @@
-class FixFriendshipForeignKey < ActiveRecord::Migration[7.0]
-    def change
-      # Drop the existing foreign key constraint
-      remove_foreign_key :friendships, :friends
-
-      # Add the correct foreign key constraint
-      add_foreign_key :friendships, :users, column: :friend_id
-    end
-  end
